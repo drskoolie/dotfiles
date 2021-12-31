dee0f0e4195036920dc424d85f273a4202cbbf1c
Al-Baraa El-Hag <a.elhag01@gmail.com>
2021-12-31 12:24:14 -0500

    more updates

--- nvim/lua/config/ultisnips/init.lua
+++ nvim/lua/config/ultisnips/init.lua
@@ -4,4 +4,7 @@ let g:UltiSnipsEditSplit="horizontal"
 let g:UltiSnipsExpandTrigger="<tab>"
 let g:UltiSnipsJumpForwardTrigger="<tab>"
 let g:UltiSnipsJumpBackwardTrigger="<s-tab>"
+
+let g:UltiSnipsSnippetDirectories=["UltiSnips"]
+
 ]])
