43088283708b884fb140b6b0c61cbb1ee8597267
Al-Baraa El-Hag <a.elhag01@gmail.com>
2022-01-08 18:28:58 -0500

    refactored ultisnips

--- nvim/lua/config/ultisnips/init.lua
+++ nvim/lua/config/ultisnips/init.lua
@@ -5,6 +5,5 @@ let g:UltiSnipsExpandTrigger="<tab>"
 let g:UltiSnipsJumpForwardTrigger="<tab>"
 let g:UltiSnipsJumpBackwardTrigger="<s-tab>"
 
-let g:UltiSnipsSnippetDirectories=["UltiSnips"]
-
+let g:UltiSnipsSnippetDirectories=["~/.dotfiles/nvim/lua/config/ultisnips/snips"]
 ]])
