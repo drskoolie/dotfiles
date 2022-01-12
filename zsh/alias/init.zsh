96b255e0dcf5db74148be2a8a6c097e4b02d4964
Al-Baraa El-Hag <a.elhag01@gmail.com>
2022-01-12 12:01:29 -0500

    restarted doing tasks

--- zsh/alias/init.zsh
+++ zsh/alias/init.zsh
@@ -1,2 +1,3 @@
 source ~/.dotfiles/zsh/alias/general.zsh
-source ~/.dotfiles/zsh/alias/taskwarrior.zsh
+# source ~/.dotfiles/zsh/alias/taskwarrior.zsh
+source ~/.dotfiles/zsh/alias/taskwarrior_new.zsh
---
+++ zsh/alias/taskwarrior_new.zsh
@@ -0,0 +1,1 @@
+alias ta='task'
