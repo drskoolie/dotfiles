cece9cb0be637fbed4cd363caccdf6cb870e6190
Al-Baraa El-Hag <a.elhag01@gmail.com>
2022-05-30 17:32:18 -0400

    adding reddit

--- i3/settings/workspace_9.zsh
+++ i3/settings/workspace_9.zsh
@@ -5,9 +5,9 @@ bindsym $mod+Shift+9 mode "cws9"
 
 set $ws9 "9:audiobook"
 set $ws91 "91:music"
-set $ws92 "92:tv"
-set $ws93 "93:youtube"
-set $ws94 "94"
+set $ws92 "92:reddit"
+set $ws93 "93:torrent"
+set $ws94 "94:youtube"
 set $ws95 "95"
 set $ws96 "96"
 set $ws97 "97"
@@ -18,8 +18,9 @@ mode "ws9" {
 
 	bindsym a workspace number $ws9; mode "default"
 	bindsym m workspace number $ws91; mode "default"
-	bindsym t workspace number $ws92; mode "default"
-	bindsym y workspace number $ws93; mode "default"
+	bindsym r workspace number $ws92; mode "default"
+	bindsym t workspace number $ws93; mode "default"
+	bindsym y workspace number $ws94; mode "default"
 
 	bindsym Escape mode "default"
 }
@@ -29,8 +30,9 @@ mode "cws9" {
 
 	bindsym a move container to workspace number $ws9; mode "default"
 	bindsym m move container to workspace number $ws91; mode "default"
-	bindsym t move container to workspace number $ws92; mode "default"
-	bindsym y move container to workspace number $ws93; mode "default"
+	bindsym r move container to workspace number $ws92; mode "default"
+	bindsym t move container to workspace number $ws93; mode "default"
+	bindsym y move container to workspace number $ws94; mode "default"
 
 	bindsym Escape mode "default"
 }
