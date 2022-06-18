28dea788fae7c8da9dc10a6705bca20edfc08ecf
Al-Baraa El-Hag <a.elhag01@gmail.com>
2022-06-18 19:56:05 -0400

    added git

--- i3/settings/workspace_9.zsh
+++ i3/settings/workspace_9.zsh
@@ -8,9 +8,9 @@ set $ws91 "91:music"
 set $ws92 "92:reddit"
 set $ws93 "93:torrent"
 set $ws94 "94:youtube"
-set $ws95 "95"
-set $ws96 "96"
-set $ws97 "97"
+set $ws95 "95:calibre"
+set $ws96 "96:stack-overflow"
+set $ws97 "97:facebook"
 set $ws98 "98"
 set $ws99 "99"
 
@@ -21,6 +21,9 @@ mode "ws9" {
 	bindsym r workspace number $ws92; mode "default"
 	bindsym t workspace number $ws93; mode "default"
 	bindsym y workspace number $ws94; mode "default"
+	bindsym c workspace number $ws95; mode "default"
+	bindsym s workspace number $ws96; mode "default"
+	bindsym f workspace number $ws97; mode "default"
 
 	bindsym Escape mode "default"
 }
@@ -33,6 +36,9 @@ mode "cws9" {
 	bindsym r move container to workspace number $ws92; mode "default"
 	bindsym t move container to workspace number $ws93; mode "default"
 	bindsym y move container to workspace number $ws94; mode "default"
+	bindsym c move container to workspace number $ws95; mode "default"
+	bindsym s move container to workspace number $ws96; mode "default"
+	bindsym f move container to workspace number $ws97; mode "default"
 
 	bindsym Escape mode "default"
 }
