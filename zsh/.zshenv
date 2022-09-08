6894e4ca731cf29e0b58d42ecbe259409e63d85a
Al-Baraa El-Hag <a.elhag01@gmail.com>
2022-09-08 12:24:31 +0000

    removed old zsh configs

--- zsh/.zshenv
+++
@@ -1,2 +0,0 @@
-# Export
-export ZDOTDIR=$HOME/.dotfiles/zsh/
--- zsh/spark.zsh
+++
@@ -1,5 +0,0 @@
-export SPARK_HOME=/opt/spark
-export PATH=$PATH:$SPARK_HOME/bin:$SPARK_HOME/sbin
-export PYSPARK_PYTHON=/usr/bin/python3
-export SPARK_MASTER=local[*]
-source /etc/profile.d/maven.sh
