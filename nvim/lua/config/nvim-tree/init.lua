448a6b75fc881ab9b3cd17bea6bdd382b2c68744
Al-Baraa El-Hag <a.elhag01@gmail.com>
2022-09-14 13:32:41 -0400

    added shortcuts for nvim-tree

--- nvim/lua/config/nvim-tree/init.lua
+++ nvim/lua/config/nvim-tree/init.lua
@@ -1,1 +1,13 @@
-require("nvim-tree").setup()
+require("nvim-tree").setup({
+	view = {
+		mappings = {
+		  list = {
+			-- { key = "h", action = "dir_up" },
+			{ key = "h", action = "close_node" },
+			{ key = "l", action = "open"},
+			{ key = "d", action = "trash"},
+			{ key = "D", action = "remove"},
+		  }
+	  }
+  }
+})
