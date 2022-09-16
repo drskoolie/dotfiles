0c96aee3165d0f84affb239e7b9b2b210951f90b
Al-Baraa El-Hag <a.elhag01@gmail.com>
2022-09-16 18:08:06 -0400

    Fixed up some of zsh installs

--- install/install-ubuntu.sh
+++ install/install-ubuntu.sh
@@ -2,6 +2,4 @@
 printf '________________________\n'
 printf 'Part I: Installing Zsh\n'
 printf '________________________\n\n'
-sudo apt install zsh
-chsh -s $(which zsh)
 source ~/dotfiles/zsh/install.sh
--- zsh/install.sh
+++ zsh/install.sh
@@ -1,4 +1,14 @@
-ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
+ZSHRC=~/.zshrc
+
+sudo apt install zsh
+chsh -s $(which zsh)
+
+if test -f "$ZSHRC"; then
+	rm ~/.zshrc
+	ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
+else
+	ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
+fi
 
 git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting ~/dotfiles/zsh/packages/zsh-syntax-highlighting
 git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ~/dotfiles/zsh/packages/zsh-autosuggestions
