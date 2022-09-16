456cbab1e6a004962034000c6774e521e7362fcf
Al-Baraa El-Hag <a.elhag01@gmail.com>
2022-09-16 18:01:34 -0400

    Finished zsh install

--- install/install-ubuntu.sh
+++ install/install-ubuntu.sh
@@ -1,5 +1,7 @@
-# This is a comment
+# Part I: Zsh
 printf '________________________\n'
 printf 'Part I: Installing Zsh\n'
 printf '________________________\n\n'
 sudo apt install zsh
+chsh -s $(which zsh)
+source ~/dotfiles/zsh/install.sh
