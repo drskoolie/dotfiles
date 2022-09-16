0ab5817aca0ae7537744273ecce15caafee61940
Al-Baraa El-Hag <a.elhag01@gmail.com>
2022-09-16 18:24:22 -0400

    Tried to fix automation of zsh packages git

--- zsh/install.sh
+++ zsh/install.sh
@@ -14,12 +14,27 @@ fi
 # Installing packages
 GIT_SYNTAX=https://github.com/zsh-users/zsh-syntax-highlighting
 FOLDER_SYNTAX=~/dotfiles/zsh/packages/zsh-syntax-highlighting
+
+GIT_AUTOSUGGESTIONS=https://github.com/zsh-users/zsh-autosuggestions
+FOLDER_AUTOSUGGESTIONS= ~/dotfiles/zsh/packages/zsh-autosuggestions
+
+GIT_POWERLEVEL10K=https://github.com/romkatv/powerlevel10k
+FOLDER_POWERLEVEL10K=~/dotfiles/zsh/packages/powerlevel10k
+
 if [ ! -d "$FOLDER_SYNTAX" ]; then
 	git clone --depth=1 $GIT_SYNTAX $FOLDER_SYNTAX
 else
-	cd "$FOLDER"
-	git pull $GIT_SYNTAX
-	cd $HOME
+	echo '$GIT_SYNTAX already cloned'
 fi
-git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ~/dotfiles/zsh/packages/zsh-autosuggestions
-git clone --depth=1 https://github.com/romkatv/powerlevel10k ~/dotfiles/zsh/packages/powerlevel10k
+
+if [ ! -d "$FOLDER_AUTOSUGGESTIONS" ]; then
+	git clone --depth=1 $GIT_AUTOSUGGESTIONS $FOLDER_SYNTAX
+else
+	echo '$GIT_AUTOSUGGESTIONS already cloned'
+fi
+
+if [ ! -d "$FOLDER_POWERLEVEL10K" ]; then
+	git clone --depth=1 $GIT_POWERLEVEL10K $FOLDER_SYNTAX
+else
+	echo '$GIT_POWERLEVEL10K already cloned'
+fi
