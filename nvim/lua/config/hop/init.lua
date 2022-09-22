b9be184091353602ee7ea5e1c9239ddc2a79c233
Al-Baraa El-Hag <a.elhag01@gmail.com>
2022-09-21 21:09:37 -0400

    Streamlined hop commands

--- nvim/lua/config/hop/init.lua
+++ nvim/lua/config/hop/init.lua
@@ -1,42 +1,17 @@
-require'hop'.setup {
-}
-
-vim.api.nvim_set_keymap('n', 'f', 
-[[<cmd>lua require'hop'.hint_char1({ current_line_only = false, })<CR>]],
-{})
-
-vim.api.nvim_set_keymap('v', 'f', 
-[[<cmd>lua require'hop'.hint_char1({ current_line_only = false, })<CR>]],
-{})
-
-vim.api.nvim_set_keymap('n', 'F',
-[[:HopPattern<CR>]],
-{})
-
-vim.api.nvim_set_keymap('v', 'F',
-[[:HopPattern<CR>]],
-{})
+require'hop'.setup { }
 
 vim.api.nvim_set_keymap('n', 't', 
-[[<cmd>lua require'hop'.hint_char1({ hint_offset = -1 })<cr>]],
+[[<cmd>lua require'hop'.hint_char1({ current_line_only = false, })<CR>]],
 {})
 
 vim.api.nvim_set_keymap('v', 't', 
-[[<cmd>lua require'hop'.hint_char1({ hint_offset = -1 })<cr>]],
+[[<cmd>lua require'hop'.hint_char1({ current_line_only = false, })<CR>]],
 {})
 
-vim.api.nvim_set_keymap('n', 'T', 
-[[<cmd>lua require'hop'.hint_char1({ hint_offset = 1 })<cr>]],
-{})
-
-vim.api.nvim_set_keymap('v', 'T', 
-[[<cmd>lua require'hop'.hint_char1({ hint_offset = 1 })<cr>]],
-{})
-
-vim.api.nvim_set_keymap('n', 's',
+vim.api.nvim_set_keymap('n', 's', 
 [[<cmd>lua require'hop'.hint_lines()<CR>]],
 {})
 
-vim.api.nvim_set_keymap('v', 's',
+vim.api.nvim_set_keymap('v', 's', 
 [[<cmd>lua require'hop'.hint_lines()<CR>]],
 {})
