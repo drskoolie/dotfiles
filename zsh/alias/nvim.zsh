394afba72d241d7b4758b93db14531e12537d235
Al-Baraa El-Hag <a.elhag01@gmail.com>
2022-09-24 17:36:07 -0400

    Added simple additions to zsh

--- zsh/alias/nvim.zsh
+++ zsh/alias/nvim.zsh
@@ -22,9 +22,11 @@ function n ()
 		t) nvim ~/dotfiles/tmux/tmux.conf;;
 
 		z) nvim ~/dotfiles/zsh/zshrc;;
+		zaa) nvim ~/dotfiles/zsh/alias/applications.zsh;;
 		zac) nvim ~/dotfiles/zsh/alias/cd.zsh;;
 		zag) nvim ~/dotfiles/zsh/alias/general.zsh;;
 		zai) nvim ~/dotfiles/zsh/alias/init.zsh;;
+		zanh) nvim ~/dotfiles/zsh/alias/nohup.zsh;;
 		zan) nvim ~/dotfiles/zsh/alias/nvim.zsh;;
 		zp) nvim ~/dotfiles/zsh/packages.zsh;;
 		*) nvim $1;;
