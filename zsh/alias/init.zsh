b5cd54408c0fe23d49d9990769dab6da84be8a9b
Al-Baraa El-Hag <a.elhag01@gmail.com>
2022-09-24 19:19:56 -0400

    Added a simple shortcut

--- zsh/alias/init.zsh
+++ zsh/alias/init.zsh
@@ -3,3 +3,4 @@ source ~/dotfiles/zsh/alias/cd.zsh
 source ~/dotfiles/zsh/alias/general.zsh
 source ~/dotfiles/zsh/alias/nohup.zsh
 source ~/dotfiles/zsh/alias/nvim.zsh
+source ~/dotfiles/zsh/alias/source.zsh
--- zsh/alias/nohup.zsh
+++ zsh/alias/nohup.zsh
@@ -16,3 +16,5 @@ function noz ()
 {
 	nohup zathura $1 >/dev/null 2>&1 &
 }
+
+alias zal='nohup zathura ~/chronicles/books/linux/advanced_linux_prog.pdf >/dev/null 2>&1 &'
