a19e15540639ee234ea21e2e949e46616cf238a6
Al-Baraa El-Hag <a.elhag01@gmail.com>
2022-09-24 17:28:22 -0400

    Starting the change of aliases

---
+++ zsh/alias/cd.zsh
@@ -0,0 +1,22 @@
+# +------------------+
+# | change directory |
+# +------------------+
+alias cdc='cd ~/chronicles/;ls'
+alias cdcb='cd ~/chronicles/books/;ls'
+
+alias cdd='cd ~/dotfiles/; ls'
+alias cddi='cd ~/dotfiles/i3; ls'
+alias cddn='cd ~/dotfiles/nvim/; ls'
+alias cddnl='cd ~/dotfiles/nvim/lua; ls'
+alias cdds='cd ~/dotfiles/setup; ls'
+alias cddt='cd ~/dotfiles/tmux; ls'
+alias cddv='cd ~/dotfiles/venv/;ls'
+alias cddz='cd ~/dotfiles/zsh; ls'
+alias cddza='cd ~/dotfiles/zsh/alias; ls'
+
+alias cdr='cd ~/research/;ls'
+alias cdrl='cd ~/research/lit/;ls'
+alias cdrli='cd ~/research/lit/ieee/;ls'
+
+alias cdx='cd ~/Documents;ls'
+
--- zsh/alias/general.zsh
+++ zsh/alias/general.zsh
@@ -8,35 +8,6 @@ alias S='scrot --select --line mode=edge'
 
 alias ls='ls -v --color'
 
-# +------------------+
-# | change directory |
-# +------------------+
-alias cdc='cd ~/chronicles/;ls'
-alias cdcb='cd ~/chronicles/books/;ls'
-
-alias cdd='cd ~/dotfiles/; ls'
-alias cddi='cd ~/dotfiles/i3; ls'
-alias cddn='cd ~/dotfiles/nvim/; ls'
-alias cddnl='cd ~/dotfiles/nvim/lua; ls'
-alias cdds='cd ~/dotfiles/setup; ls'
-alias cddt='cd ~/dotfiles/tmux; ls'
-alias cddv='cd ~/dotfiles/venv/;ls'
-alias cddz='cd ~/dotfiles/zsh; ls'
-alias cddza='cd ~/dotfiles/zsh/alias; ls'
-
-alias cdr='cd ~/research/;ls'
-alias cdrl='cd ~/research/lit/;ls'
-alias cdrli='cd ~/research/lit/ieee/;ls'
-
-alias cdx='cd ~/Documents;ls'
-
-# +-----+
-# | fzf |
-# +-----+
-alias fzfp="fzf --preview 'batcat --style=numbers --color=always --style=numbers --line-range :500 {}'\
-	--bind shift-up:preview-page-up,shift-down:preview-page-down\
-	|xclip -selection clipboard"
-
 # +--------+
 # | neovim |
 # +--------+
