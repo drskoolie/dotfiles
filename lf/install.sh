1ba1db1940d2f67f5b8fdeda1c28eb0315ba3f41
Al-Baraa El-Hag <a.elhag01@gmail.com>
2022-09-25 09:49:23 -0400

    Setting up lf

--- lf/install.sh
+++ lf/install.sh
@@ -1,6 +1,10 @@
+rm /usr/local/bin/lf
+
 if test -d ~/.config/lf; then
 	rm -rf ~/.config/lf
 	ln -s ~/dotfiles/lf ~/.config/lf
 else
 	ln -s ~/dotfiles/lf ~/.config/lf
 fi
+
+ln -s ~/dotfiles/lf/lf_bin ~/usr/local/bin/lf
