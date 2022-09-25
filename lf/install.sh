221e98859c8c01b96f562a2944206c3d7ac0b091
Al-Baraa El-Hag <a.elhag01@gmail.com>
2022-09-25 09:51:15 -0400

    Tried to fix lf

--- lf/install.sh
+++ lf/install.sh
@@ -1,5 +1,3 @@
-rm /usr/local/bin/lf
-
 if test -d ~/.config/lf; then
 	rm -rf ~/.config/lf
 	ln -s ~/dotfiles/lf ~/.config/lf
@@ -7,4 +5,4 @@ else
 	ln -s ~/dotfiles/lf ~/.config/lf
 fi
 
-ln -s ~/dotfiles/lf/lf_bin ~/usr/local/bin/lf
+cp ~/dotfiles/lf/lf_bin /usr/local/bin/lf
