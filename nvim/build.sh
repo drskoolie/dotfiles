1a6ba4e902ab26b1aec1b01f9c3a83ba86557b8b
Al-Baraa El-Hag <a.elhag01@gmail.com>
2022-09-27 13:34:42 +0000

    added a build file for neovim

---
+++ nvim/build.sh
@@ -0,0 +1,5 @@
+sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen
+git clone https://github.com/neovim/neovim ~/neovim
+cd ~/neovim
+make CMAKE_BUILD_TYPE=RelWithDebInfo
+sudo make install
