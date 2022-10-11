3cd651fb47dd01a7780cee64d7216401befba5cb
Al-Baraa El-Hag <a.elhag01@gmail.com>
2022-10-10 21:42:36 -0400

    Added just a little

--- zsh/alias/nohup.zsh
+++ zsh/alias/nohup.zsh
@@ -22,5 +22,6 @@ function noz ()
 	nohup zathura $1 >/dev/null 2>&1 &
 }
 
+alias nozd='nohup zathura ~/chronicles/books/ml/deep_learning_ian.pdf >/dev/null 2>&1 &'
 alias nozc='nohup zathura ~/chronicles/books/c/modern_approach_2ed_p1.pdf >/dev/null 2>&1 &'
 alias nozp='nohup zathura ~/chronicles/books/python/algos_data_structures.pdf >/dev/null 2>&1 &'
