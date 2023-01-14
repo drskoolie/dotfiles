bdede5b0594f9f431461befea3cfa8670d0ee2e6
Al-Baraa El-Hag <a.elhag01@gmail.com>
2023-01-14 17:40:20 -0500

    Added ipython

---
+++ ipython/install.sh
@@ -0,0 +1,8 @@
+if test -d ~/.ipython/; then
+	rm -rf ~/.ipython
+	ln -s ~/dotfiles/ipython/ ~/.ipython/
+	echo "ipython set up"
+else
+	ln -s ~/dotfiles/ipython/ ~/.ipython
+	echo "ipython set up"
+fi
