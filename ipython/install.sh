1778180a803c6e85e9106d3b9c2f31218182ce3c
Al-Baraa El-Hag <a.elhag01@gmail.com>
2023-01-22 06:04:42 -0500

    Fixed location error

--- ipython/install.sh
+++ ipython/install.sh
@@ -1,8 +1,8 @@
-if test -d ~/.ipython/; then
+if test -d ~/.ipython; then
 	rm -rf ~/.ipython
-	ln -s ~/dotfiles/ipython/ ~/.ipython/
+	ln -s ~/dotfiles/ipython ~/.ipython
 	echo "ipython set up"
 else
-	ln -s ~/dotfiles/ipython/ ~/.ipython
+	ln -s ~/dotfiles/ipython ~/.ipython
 	echo "ipython set up"
 fi
