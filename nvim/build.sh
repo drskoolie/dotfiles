7084325ac464eb37bfd0497246c09bd4f0bd8ea5
Al-Baraa El-Hag <a.elhag01@gmail.com>
2023-01-22 00:18:22 -0500

    Fixed up slight issues with cd and -y argument

--- nvim/build.sh
+++ nvim/build.sh
@@ -1,5 +1,6 @@
-sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen
+sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen -y
 git clone https://github.com/neovim/neovim ~/neovim
 cd ~/neovim
 make CMAKE_BUILD_TYPE=RelWithDebInfo
 sudo make install
+cd -
