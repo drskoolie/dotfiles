c2945cf33e89900235674c587e62d329b7cf24ec
Al-Baraa El-Hag <a.elhag01@gmail.com>
2023-01-22 06:06:16 -0500

    Added autoinstall ipython

--- ipython/install.sh
+++ ipython/install.sh
@@ -1,3 +1,6 @@
+source ~/dotfiles/venv/venv_global/bin/activate
+pip install ipython rich
+
 if test -d ~/.ipython; then
 	rm -rf ~/.ipython
 	ln -s ~/dotfiles/ipython ~/.ipython
@@ -7,5 +10,3 @@ else
 	echo "ipython set up"
 fi
 
-source ~/dotfiles/venv/venv_global/bin/activate
-pip install rich
