9cd35b33ad62a68e09ca6846126fbf2a996325fd
Al-Baraa El-Hag <a.elhag01@gmail.com>
2023-01-25 12:21:10 -0500

    Simple shell for termux

---
+++ install/termux_01_base.sh
@@ -0,0 +1,10 @@
+# Part III: Symliking
+printf '________________________\n'
+printf 'Part 3: Symlinking\n'
+printf '________________________\n\n'
+mkdir -p ~/.config
+source ~/dotfiles/git/install.sh
+source ~/dotfiles/lf/install.sh
+source ~/dotfiles/picom/install.sh
+source ~/dotfiles/tmux/install.sh
+source ~/dotfiles/zsh/install.sh
