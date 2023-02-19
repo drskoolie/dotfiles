e27798fdcf9900c9568ba6f42a8a83abb948ba5b
Al-Baraa El-Hag <a.elhag01@gmail.com>
2023-02-19 09:30:05 -0500

    Simple changes

--- screenlayout/5Mons.sh
+++ screenlayout/5Mons.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DisplayPort-0 --primary --mode 1680x1050 --pos 8720x0 --rotate normal --output DisplayPort-1 --mode 1920x1080 --pos 1680x0 --rotate normal --output DisplayPort-2 --mode 1680x1050 --pos 0x0 --rotate normal --output HDMI-A-0 --mode 3440x1440 --pos 5280x0 --rotate normal --output DVI-D-0 --mode 1680x1050 --pos 3600x0 --rotate normal
+xrandr --output DisplayPort-0 --primary --mode 1680x1050 --pos 8720x390 --rotate normal --output DisplayPort-1 --mode 1920x1080 --pos 1680x360 --rotate normal --output DisplayPort-2 --mode 1680x1050 --pos 0x390 --rotate normal --output HDMI-A-0 --mode 3440x1440 --pos 5280x0 --rotate normal --output DVI-D-0 --mode 1680x1050 --pos 3600x390 --rotate normal
--- venv/source.zsh
+++ venv/source.zsh
@@ -1,4 +1,7 @@
 python3 -m venv venv_global
 python3 -m venv venv_neovim
+python3 -m venv venv_ml
+
 source venv_neovim/bin/activate
 pip install pynvim black
+deactivate
