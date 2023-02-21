c1eee91980f3a74740a81a480d37803f28d426cd
Al-Baraa El-Hag <a.elhag01@gmail.com>
2023-02-21 01:25:11 -0500

    Just finished this

--- lf/previewer.sh
+++ lf/previewer.sh
@@ -5,5 +5,5 @@ case "$1" in
     *.rar) unrar l "$1";;
     *.7z) 7z l "$1";;
     *.pdf) pdftotext "$1" -;;
-    *) bat --color always "$1" ;;
+    *) batcat --color --style=plain always "$1" ;;
 esac
