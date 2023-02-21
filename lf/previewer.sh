cfc89d0bc35feb629feafd9799b21959563a0308
Al-Baraa El-Hag <a.elhag01@gmail.com>
2023-02-21 01:22:11 -0500

    Added previewing

---
+++ lf/previewer.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+case "$1" in
+    *.tar*) tar tf "$1";;
+    *.zip) unzip -l "$1";;
+    *.rar) unrar l "$1";;
+    *.7z) 7z l "$1";;
+    *.pdf) pdftotext "$1" -;;
+    *) bat --color always "$1" ;;
+esac
