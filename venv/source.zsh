1fe8d2dbf6307935393dd7c86d2c1a66dc7eccca
Al-Baraa El-Hag <a.elhag01@gmail.com>
2023-02-21 15:11:15 -0500

    Added a bit more stuff to venv

--- venv/source.zsh
+++ venv/source.zsh
@@ -5,3 +5,11 @@ python3 -m venv venv_ml
 source venv_neovim/bin/activate
 pip install pynvim black
 deactivate
+
+source venv_global/bin/activate
+pip install ipython rich
+deactivate
+
+source venv_ml/bin/activate
+pip install ipython rich numpy matplotlib pandas scikit-learn
+deactivate
