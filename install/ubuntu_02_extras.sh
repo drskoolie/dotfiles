b240e9fbe2e3f0cc04e2a0b5b926878887fcca54
Al-Baraa El-Hag <a.elhag01@gmail.com>
2023-02-21 20:48:48 -0500

    Simple changes

--- install/ubuntu_02_extras.sh
+++ install/ubuntu_02_extras.sh
@@ -2,4 +2,4 @@ printf '________________________\n'
 printf '(02) Ubuntu - Extras\n'
 printf '________________________\n\n'
 sleep 1
-sudo apt install boxes bat cmus figlet lolcat mpv poppler-utils simplescreenrecorder trash-cli xclip zathura zoxide -y
+sudo apt install boxes bat cmus figlet lolcat mediainfo mpv poppler-utils simplescreenrecorder trash-cli xclip zathura zoxide -y
--- lf/previewer.sh
+++ lf/previewer.sh
@@ -5,8 +5,11 @@ case "$1" in
     *.zip) unzip -l "$1";;
     *.rar) unrar l "$1";;
     *.7z) 7z l "$1";;
+
     *.pdf) pdftotext "$1" -;;
 	*.pptx) unzip -qc "$1" | grep -oP '(?<=\<a:t\>).*?(?=\</a:t\>)';;
+
+	*.png|*.jpg|*.jpeg|*.mkv|*.mp4|*.3gp) mediainfo "$1";;
 
     *) bat -n --color always "$1" ;;
 esac
