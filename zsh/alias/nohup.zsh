dd8ffe33abeef2b687a447b214fe54d9fc6b590e
Al-Baraa El-Hag <a.elhag01@gmail.com>
2023-02-21 19:48:02 -0500

    Added an alias for opening git book

--- zsh/alias/nohup.zsh
+++ zsh/alias/nohup.zsh
@@ -21,6 +21,4 @@ function noz ()
 	nohup zathura $1 >/dev/null 2>&1 &
 }
 
-alias noza='nohup zathura ~/chronicles/books/assembly/step-by-step-linux.pdf >/dev/null 2>&1 &'
-alias nozc='nohup zathura ~/chronicles/books/c/modern_approach_2ed_p1.pdf >/dev/null 2>&1 &'
-alias nozp='nohup zathura ~/chronicles/books/python/algos_data_structures.pdf >/dev/null 2>&1 &'
+alias nozg='nohup zathura ~/github/books/git/progit.pdf >/dev/null 2>&1 &'
