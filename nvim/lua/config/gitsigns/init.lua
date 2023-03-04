5f08ef969c3ce5542d161763032d3fe1d8b147c0
Al-Baraa El-Hag <a.elhag01@gmail.com>
2023-03-04 04:01:39 -0500

    Simple changes to files

--- nvim/lua/config/gitsigns/init.lua
+++ nvim/lua/config/gitsigns/init.lua
@@ -1,9 +1,9 @@
 require('gitsigns').setup 
 {
   signs = {
-    add          = { text = '+' },
+    add          = { text = '│' },
     change       = { text = '│' },
-    delete       = { text = '-' },
+    delete       = { text = '_' },
     topdelete    = { text = '‾' },
     changedelete = { text = '~' },
     untracked    = { text = '┆' },
