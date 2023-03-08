a6ddb38bb464765d4f582782ba0d8b759f238eec
Al-Baraa El-Hag <a.elhag01@gmail.com>
2023-03-08 14:19:25 -0500

    Moved variables out

--- zsh/packages/packages_install.sh
+++ zsh/packages/packages_install.sh
@@ -1,39 +1,19 @@
-# Installing zsh
-chsh -s $(which zsh)
-
-# Linking zshrc
-ZSHRC=~/.zshrc
-if test -f "$ZSHRC"; then
-	rm ~/.zshrc
-	ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
+# Zsh Autosuggestions
+if [ ! -d ~/dotfiles/zsh/packages/zsh-autosuggestions ]; then
+	git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ~/dotfiles/zsh/packages/zsh-autosuggestions
 else
-	ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
+	echo "https://github.com/zsh-users/zsh-autosuggestions already cloned"
 fi
 
-# Installing packages
-GIT_SYNTAX=https://github.com/zsh-users/zsh-syntax-highlighting
-FOLDER_SYNTAX=~/dotfiles/zsh/packages/zsh-syntax-highlighting
-
-GIT_AUTOSUGGESTIONS=https://github.com/zsh-users/zsh-autosuggestions
-FOLDER_AUTOSUGGESTIONS=~/dotfiles/zsh/packages/zsh-autosuggestions
-
-GIT_POWERLEVEL10K=https://github.com/romkatv/powerlevel10k
-FOLDER_POWERLEVEL10K=~/dotfiles/zsh/packages/powerlevel10k
-
-if [ ! -d "$FOLDER_SYNTAX" ]; then
-	git clone --depth=1 $GIT_SYNTAX $FOLDER_SYNTAX
+# Zsh Syntax Highlighting
+if [ ! -d ~/dotfiles/zsh/packages/zsh-syntax-highlighting ]; then
+	git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting ~/dotfiles/zsh/packages/zsh-syntax-highlighting
 else
-	echo "$GIT_SYNTAX already cloned"
+	echo "https://github.com/zsh-users/zsh-syntax-highlighting  already cloned"
 fi
 
-if [ ! -d "$FOLDER_AUTOSUGGESTIONS" ]; then
-	git clone --depth=1 $GIT_AUTOSUGGESTIONS $FOLDER_AUTOSUGGESTIONS
+if [ ! -d ~/dotfiles/zsh/packages/powerlevel10k ]; then
+	git clone --depth=1 https://github.com/romkatv/powerlevel10k ~/dotfiles/zsh/packages/powerlevel10k
 else
-	echo "$GIT_AUTOSUGGESTIONS already cloned"
-fi
-
-if [ ! -d "$FOLDER_POWERLEVEL10K" ]; then
-	git clone --depth=1 $GIT_POWERLEVEL10K $FOLDER_POWERLEVEL10K
-else
-	echo "$GIT_POWERLEVEL10K already cloned"
+	echo "https://github.com/romkatv/powerlevel10k already cloned"
 fi
