726f0f8cc3af6d509a74485db25cdcd8b31a4363
Al-Baraa El-Hag <a.elhag01@gmail.com>
2023-03-08 14:25:55 -0500

    Converted it to a function

--- zsh/packages/packages_install.sh
+++ zsh/packages/packages_install.sh
@@ -1,19 +1,11 @@
-# Zsh Autosuggestions
-if [ ! -d ~/dotfiles/zsh/packages/zsh-autosuggestions ]; then
-	git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ~/dotfiles/zsh/packages/zsh-autosuggestions
-else
-	echo "https://github.com/zsh-users/zsh-autosuggestions already cloned"
-fi
+zsh_clone() {
+	if [ ! -d $1 ]; then
+		git clone --depth=1 $2 $1
+	else
+		echo "$2 already cloned"
+	fi
+}
 
-# Zsh Syntax Highlighting
-if [ ! -d ~/dotfiles/zsh/packages/zsh-syntax-highlighting ]; then
-	git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting ~/dotfiles/zsh/packages/zsh-syntax-highlighting
-else
-	echo "https://github.com/zsh-users/zsh-syntax-highlighting  already cloned"
-fi
-
-if [ ! -d ~/dotfiles/zsh/packages/powerlevel10k ]; then
-	git clone --depth=1 https://github.com/romkatv/powerlevel10k ~/dotfiles/zsh/packages/powerlevel10k
-else
-	echo "https://github.com/romkatv/powerlevel10k already cloned"
-fi
+zsh_clone ~/dotfiles/zsh/packages/zsh-autosuggestions https://github.com/zsh-users/zsh-autosuggestions 
+zsh_clone ~/dotfiles/zsh/packages/zsh-syntax-highlighting https://github.com/zsh-users/zsh-syntax-highlighting 
+zsh_clone ~/dotfiles/zsh/packages/powerlevel10k https://github.com/romkatv/powerlevel10k
