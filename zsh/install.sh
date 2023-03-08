c315d07776556a6b8eee1dd0d377c518e6cd8e66
Al-Baraa El-Hag <a.elhag01@gmail.com>
2023-03-08 14:11:39 -0500

    Moved files to packages subdirectory

--- zsh/install.sh
+++ zsh/install.sh
@@ -10,30 +10,4 @@ else
 	ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
 fi
 
-# Installing packages
-GIT_SYNTAX=https://github.com/zsh-users/zsh-syntax-highlighting
-FOLDER_SYNTAX=~/dotfiles/zsh/packages/zsh-syntax-highlighting
-
-GIT_AUTOSUGGESTIONS=https://github.com/zsh-users/zsh-autosuggestions
-FOLDER_AUTOSUGGESTIONS=~/dotfiles/zsh/packages/zsh-autosuggestions
-
-GIT_POWERLEVEL10K=https://github.com/romkatv/powerlevel10k
-FOLDER_POWERLEVEL10K=~/dotfiles/zsh/packages/powerlevel10k
-
-if [ ! -d "$FOLDER_SYNTAX" ]; then
-	git clone --depth=1 $GIT_SYNTAX $FOLDER_SYNTAX
-else
-	echo "$GIT_SYNTAX already cloned"
-fi
-
-if [ ! -d "$FOLDER_AUTOSUGGESTIONS" ]; then
-	git clone --depth=1 $GIT_AUTOSUGGESTIONS $FOLDER_AUTOSUGGESTIONS
-else
-	echo "$GIT_AUTOSUGGESTIONS already cloned"
-fi
-
-if [ ! -d "$FOLDER_POWERLEVEL10K" ]; then
-	git clone --depth=1 $GIT_POWERLEVEL10K $FOLDER_POWERLEVEL10K
-else
-	echo "$GIT_POWERLEVEL10K already cloned"
-fi
+source ~/dotfiles/zsh/packages/packages_install.sh
