baa039069cb454d9ed4bb8ee67c1a7372d11ef37
Al-Baraa El-Hag <a.elhag01@gmail.com>
2023-03-14 01:59:21 -0400

    Add new zsh

--- zsh/alias/nohup.zsh
+++ zsh/alias/nohup.zsh
@@ -32,4 +32,5 @@ function noz ()
 	nohup zathura $1 >/dev/null 2>&1 &
 }
 
+alias nozc='nohup zathura ~/github/books/c/modern_approach_2ed_p1.pdf >/dev/null 2>&1 &'
 alias nozg='nohup zathura ~/github/books/git/progit.pdf >/dev/null 2>&1 &'
