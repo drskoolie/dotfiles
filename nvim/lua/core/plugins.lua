c094a96745380652c098a101971a74d592af2e14
Al-Baraa El-Hag <a.elhag01@gmail.com>
2023-03-19 20:35:37 -0400

    Remove lf

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -29,26 +29,26 @@ return require('packer').startup(function()
   -- +--------------+
   -- | file browser |
   -- +--------------+
-  use(
-      {
-          "lmburns/lf.nvim",
-          config = function()
-            -- This feature will not work if the plugin is lazy-loaded
-            vim.g.lf_netrw = 1
-  
-            require("lf").setup(
-                {
-                    escape_quit = false,
-                    border = "rounded",
-                    highlights = {FloatBorder = {guifg = require("kimbox.palette").colors.magenta}}
-                }
-            )
-  
-            vim.keymap.set("n", "<C-o>", ":Lf<CR>")
-          end,
-          requires = {"plenary.nvim", "toggleterm.nvim"}
-      }
-  )
+  -- use(
+  --     {
+  --         "lmburns/lf.nvim",
+  --         config = function()
+  --           -- This feature will not work if the plugin is lazy-loaded
+  --           vim.g.lf_netrw = 1
+  -- 
+  --           require("lf").setup(
+  --               {
+  --                   escape_quit = false,
+  --                   border = "rounded",
+  --                   highlights = {FloatBorder = {guifg = require("kimbox.palette").colors.magenta}}
+  --               }
+  --           )
+  -- 
+  --           vim.keymap.set("n", "<C-o>", ":Lf<CR>")
+  --         end,
+  --         requires = {"plenary.nvim", "toggleterm.nvim"}
+  --     }
+  -- )
 
   use {"akinsho/toggleterm.nvim", tag = '*', config = function()
     require("toggleterm").setup()
