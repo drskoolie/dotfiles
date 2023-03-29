d6423491326c351189f29b8f9fc7cd11d4af4928
Al-Baraa El-Hag <a.elhag01@gmail.com>
2023-03-29 04:33:16 -0400

    I lied, an alphabetical reorganizing of neo-tree

--- nvim/lua/config/nvim-neo-tree/init.lua
+++ nvim/lua/config/nvim-neo-tree/init.lua
@@ -88,24 +88,13 @@ require("neo-tree").setup({
                 "toggle_node", 
                 nowait = false, -- disable `nowait` if you have existing combos starting with this char that you want to use 
             },
+            ["<"] = "prev_source",
+            [">"] = "next_source",
+            ["?"] = "show_help",
             ["<2-LeftMouse>"] = "open",
             ["<cr>"] = "open",
-			["l"] = "open",
-            ["<esc>"] = "revert_preview",
-            ["P"] = { "toggle_preview", config = { use_float = true } },
-            -- ["l"] = "focus_preview",
-            ["S"] = "open_split",
-            ["s"] = "open_vsplit",
-            -- ["S"] = "split_with_window_picker",
-            -- ["s"] = "vsplit_with_window_picker",
-            ["t"] = "open_tabnew",
             -- ["<cr>"] = "open_drop",
-            -- ["t"] = "open_tab_drop",
-            ["w"] = "open_with_window_picker",
-            ["C"] = "close_node",
-            -- ['C'] = 'close_all_subnodes',
-            ["z"] = "close_all_nodes",
-            --["Z"] = "expand_all_nodes",
+            ["<esc>"] = "revert_preview",
             ["a"] = { 
               "add",
               -- this command supports BASH style brace expansion ("x{a,b,c}" -> xa,xb,xc). see `:h neo-tree-file-actions` for details
@@ -115,24 +104,29 @@ require("neo-tree").setup({
               }
             },
             ["A"] = "add_directory", -- also accepts the optional config.show_path option like "add". this also supports BASH style brace expansion.
-            ["d"] = "delete",
-            ["r"] = "rename",
-            ["y"] = "copy_to_clipboard",
-            ["x"] = "cut_to_clipboard",
-            ["p"] = "paste_from_clipboard",
             ["c"] = "copy", -- takes text input for destination, also accepts the optional config.show_path option like "add":
-            -- ["c"] = {
-            --  "copy",
-            --  config = {
-            --    show_path = "none" -- "none", "relative", "absolute"
-            --  }
-            --}
+            ["C"] = "close_node",
+            -- ['C'] = 'close_all_subnodes',
+            ["d"] = "delete",
+			["l"] = "open",
+            -- ["l"] = "focus_preview",
             ["m"] = "move", -- takes text input for destination, also accepts the optional config.show_path option like "add".
+            ["p"] = "paste_from_clipboard",
+            ["P"] = { "toggle_preview", config = { use_float = true } },
             ["q"] = "close_window",
+            ["r"] = "rename",
             ["R"] = "refresh",
-            ["?"] = "show_help",
-            ["<"] = "prev_source",
-            [">"] = "next_source",
+            ["s"] = "open_vsplit",
+            -- ["s"] = "vsplit_with_window_picker",
+            ["S"] = "open_split",
+            -- ["S"] = "split_with_window_picker",
+            ["t"] = "open_tabnew",
+            -- ["t"] = "open_tab_drop",
+            ["w"] = "open_with_window_picker",
+            ["x"] = "cut_to_clipboard",
+            ["y"] = "copy_to_clipboard",
+            ["z"] = "close_all_nodes",
+            --["Z"] = "expand_all_nodes",
           }
         },
         nesting_rules = {},
