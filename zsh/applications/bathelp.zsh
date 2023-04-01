fa60d33fc7ddf24901148d67538b0bbb25dff4b4
Al-Baraa El-Hag <a.elhag01@gmail.com>
2023-04-01 01:08:12 -0400

    Simple curtailment of zsh

--- zsh/applications/bathelp.zsh
+++
@@ -1,6 +0,0 @@
-alias bathelp='bat --plain --language=help'
-
-function help () 
-{
-    "$@" --help 2>&1 | bathelp
-}
--- zsh/help.zsh
+++
@@ -1,4 +0,0 @@
-unalias run-help 2>/dev/null
-autoload run-help
-HELPDIR=/path/to/zsh_help_directory
-alias help=run-help
