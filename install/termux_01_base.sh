db532509abac8808e78ca5acff3f6e97965b98bf
Al-Baraa El-Hag <a.elhag01@gmail.com>
2023-07-09 12:43:12 -0400

    Added a bit more to termux

--- install/termux_01_base.sh
+++ install/termux_01_base.sh
@@ -1,3 +1,5 @@
+pkg install clang git lf nodejs-lts python neovim tmux zoxide zsh 
+
 # Part III: Symliking
 printf '________________________\n'
 printf 'Part 3: Symlinking\n'
@@ -5,6 +7,5 @@ printf '________________________\n\n'
 mkdir -p ~/.config
 source ~/dotfiles/git/install.sh
 source ~/dotfiles/lf/install.sh
-source ~/dotfiles/picom/install.sh
 source ~/dotfiles/tmux/install.sh
 source ~/dotfiles/zsh/install.sh
--- install/ubuntu_02_extras.sh
+++ install/ubuntu_02_extras.sh
@@ -2,4 +2,4 @@ printf '________________________\n'
 printf '(02) Ubuntu - Extras\n'
 printf '________________________\n\n'
 sleep 1
-sudo apt install boxes bat cmus figlet lolcat mediainfo mpv poppler-utils python3.10-tk simplescreenrecorder trash-cli xclip zathura zoxide -y
+sudo apt install boxes bat cmus figlet lolcat mediainfo mpv pamix poppler-utils python3.10-tk simplescreenrecorder trash-cli xclip zathura zoxide -y
