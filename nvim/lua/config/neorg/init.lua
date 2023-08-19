898fdbdd32c9b9e136752bca54625d18580abbf5
Al-Baraa El-Hag <a.elhag01@gmail.com>
2023-08-19 18:12:31 -0400

    Neorg add

--- nvim/lua/config/neorg/init.lua
+++ nvim/lua/config/neorg/init.lua
@@ -1,13 +1,18 @@
 require('neorg').setup {
+	build = ":Neorg sync-parsers",
     load = {
+
+		["core.concealer"] = {},
         ["core.defaults"] = {},
         ["core.dirman"] = {
             config = {
                 workspaces = {
-                    work = "~/github/neorg-notes/work",
                     home = "~/github/neorg-notes/home",
-                }
-            }
-        }
+                    work = "~/github/neorg-notes/work",
+                }}},
+		["core.keybinds"] = {
+			config = {
+				default_keybinds = false,
+			}},
     }
 }
