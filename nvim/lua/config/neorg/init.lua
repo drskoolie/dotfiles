0c2c703b95d7a60a57a19f042d9df33c52bd990d
Al-Baraa El-Hag <a.elhag01@gmail.com>
2023-08-20 15:26:24 -0400

    More neorg

--- nvim/lua/config/neorg/init.lua
+++ nvim/lua/config/neorg/init.lua
@@ -8,6 +8,12 @@ require('neorg').setup {
 				icon_preset = "diamond",
 			}},
         ["core.defaults"] = {},
+		["core.journal"] = {
+			config = {
+				journal_folder ="journal",
+				strategy = "flat",
+			},
+		},
         ["core.dirman"] = {
             config = {
                 workspaces = {
@@ -17,6 +23,13 @@ require('neorg').setup {
 		["core.keybinds"] = {
 			config = {
 				default_keybinds = false,
+
+				hook = function(keybinds)
+					keybinds.remap_event("norg", "n", "<CR>", "core.esupports.hop.hop-link")
+				end,
+                    -- { "gd", "core.esupports.hop.hop-link", opts = { desc = "Jump to Link" } },
+                    -- { "gf", "core.esupports.hop.hop-link", opts = { desc = "Jump to Link" } },
+                    -- { "gF", "core.esupports.hop.hop-link", opts = { desc = "Jump to Link" } },
 			}},
     }
 }
