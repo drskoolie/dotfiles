7c813a4c29722c3193895f3042cb2058dccd1535
Al-Baraa El-Hag <a.elhag01@gmail.com>
2023-09-01 11:58:58 -0400

    Remove OpenSCAD

--- OpenSCAD/install.sh
+++
@@ -1,7 +0,0 @@
-FILE_OPENSCAD=~/.config/OpenSCAD
-if test -f "$FILE_OPENSCAD"; then
-	rm ~/.config/OpenSCAD/
-	ln -s ~/dotfiles/OpenSCAD/ ~/.config/
-else
-	ln -s ~/dotfiles/OpenSCAD/ ~/.config/
-fi
