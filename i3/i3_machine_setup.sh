5a308572da249d01dd7dfa37c353c15266467bd1
Al-Baraa El-Hag <a.elhag01@gmail.com>
2023-09-25 12:14:02 -0400

    Will get back to this soon

--- i3/i3_machine_setup.sh
+++ i3/i3_machine_setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
+	# i3-msg "bindsym XF86RotateWindows exec xrandr --output LVDS-1 --rotate inverted"
+	# i3-msg "bindsym XF86TaskPane exec xrandr --output LVDS-1 --rotate normal"
 if [ "$MACHINE_NAME" == "x220T" ]; then
 	i3-msg "exec --no-startup-id 'xmodmap ~/.Xmodmap'"
-	i3-msg "set bindsym XF86RotateWindows exec xrandr --output LVDS-1 --rotate inverted"
-	i3-msg "set bindsym XF86TaskPane exec xrandr --output LVDS-1 --rotate normal":
 
 elif [ "$MACHINE_NAME" == "somkthine" ]; then
 	i3-msg "set $mod Mod1"
