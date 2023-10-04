7e639863a937a469d906442ff7dfdf5bdbc17dc0
Al-Baraa El-Hag <a.elhag01@gmail.com>
2023-10-04 08:39:08 -0400

    Add new arch stuff

---
+++ install/arch_01.sh
@@ -0,0 +1,15 @@
+# Part III: Symliking
+printf '________________________\n'
+printf 'Part 3: Symlinking\n'
+printf '________________________\n\n'
+mkdir -p ~/.config
+source ~/dotfiles/fonts/source.zsh
+source ~/dotfiles/git/install.sh
+source ~/dotfiles/i3/install.sh
+source ~/dotfiles/lf/install.sh
+source ~/dotfiles/mpv/install.sh
+source ~/dotfiles/picom/install.sh
+source ~/dotfiles/tmux/install.sh
+source ~/dotfiles/xinitrc/install.sh
+source ~/dotfiles/zathura/install.sh
+source ~/dotfiles/zsh/install.sh
---
+++ install/arch_02.sh
@@ -0,0 +1,5 @@
+printf '________________________\n'
+printf '(02) Ubuntu - Extras\n'
+printf '________________________\n\n'
+sleep 1
+sudo pacman -Syu boxes bat cmus figlet lolcat mediainfo mpv pamix poppler-utils python3.10-tk simplescreenrecorder trash-cli xclip zathura zoxide -y
