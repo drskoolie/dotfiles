fd792c402283c90fbc851759a9522082f6828e70
Al-Baraa El-Hag <a.elhag01@gmail.com>
2023-12-02 10:13:56 -0500

    Add

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -68,11 +68,12 @@ return require('packer').startup(function()
   -- | hop |
   -- +-----+
   use {
-	  "phaazon/hop.nvim",
-	  branch = 'v2',
-	  config = function()
-		  require'hop'.setup {}
-	  end
+    'smoka7/hop.nvim',
+    tag = '*', -- optional but strongly recommended
+    config = function()
+      -- you can configure Hop the way you like here; see :h hop-config
+      require'hop'.setup {}
+    end
   }
 
   -- +------+
