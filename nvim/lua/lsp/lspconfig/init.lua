779ecc86bcb80f240b996833d047a8d334892ebf
Al-Baraa El-Hag <a.elhag01@gmail.com>
2024-02-16 15:11:56 -0500

    Finish setting up lsp

--- nvim/lua/lsp/lspconfig/init.lua
+++ nvim/lua/lsp/lspconfig/init.lua
@@ -7,6 +7,7 @@ vim.api.nvim_create_autocmd("FileType", {
         lspconfig.pyright.setup{}
 		lspconfig.ruff_lsp.setup{
 			cmd = {"ruff-lsp"},
+			root_dir = lspconfig.util.root_pattern(".git", "setup.py", "pyproject.toml", ".ruff-lsp.toml", "."),
 		}
 
 		-- require'lspconfig'.ccls.setup{}
