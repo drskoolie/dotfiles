04a0d9a15268f87f351dd6c5c821dabdec6e818b
Al-Baraa El-Hag <a.elhag01@gmail.com>
2024-02-16 15:20:09 -0500

    Remove nvim autocommand for LSP

--- nvim/lua/lsp/lspconfig/init.lua
+++ nvim/lua/lsp/lspconfig/init.lua
@@ -1,15 +1,8 @@
 local lspconfig = require('lspconfig')
 
-vim.api.nvim_create_autocmd("FileType", {
-    pattern = "python",
-    callback = function()
-
-        lspconfig.pyright.setup{}
-		lspconfig.ruff_lsp.setup{
-			cmd = {"ruff-lsp"},
-			root_dir = lspconfig.util.root_pattern(".git", "setup.py", "pyproject.toml", ".ruff-lsp.toml", "."),
-		}
-
-		-- require'lspconfig'.ccls.setup{}
-    end,
-})
+lspconfig.pyright.setup{}
+lspconfig.ruff_lsp.setup{
+	cmd = {"ruff-lsp"},
+	root_dir = lspconfig.util.root_pattern(".git", "setup.py", "pyproject.toml", ".ruff-lsp.toml", "."),
+}
+-- require'lspconfig'.ccls.setup{}
