e67c7c30aec4efd504f7b73b75e36bfcf0c99e53
Al-Baraa El-Hag <a.elhag01@gmail.com>
2024-02-19 16:44:38 -0500

    Fix bug

--- nvim/lua/lsp/mason/init.lua
+++ nvim/lua/lsp/mason/init.lua
@@ -1,8 +1,9 @@
 require("mason").setup()
 require("mason-lspconfig").setup()
 
-require('lspconfig').pyright.setup{}
-require('lspconfig').ruff_lsp.setup{
+local lspconfig = require('lspconfig')
+lspconfig.pyright.setup{}
+lspconfig.ruff_lsp.setup{
 	cmd = {"ruff-lsp"},
 	root_dir = lspconfig.util.root_pattern(".git", "setup.py", "pyproject.toml", ".ruff-lsp.toml", "."),
 }
