dd9ea25c23338595d199edbed914cce1364a9700
Al-Baraa El-Hag <a.elhag01@gmail.com>
2024-02-19 16:38:59 -0500

    Disable lspconfig's setup of servers

--- nvim/lua/lsp/lspconfig/init.lua
+++ nvim/lua/lsp/lspconfig/init.lua
@@ -1,8 +1,7 @@
 local lspconfig = require('lspconfig')
 
-lspconfig.pyright.setup{}
-lspconfig.ruff_lsp.setup{
-	cmd = {"ruff-lsp"},
-	root_dir = lspconfig.util.root_pattern(".git", "setup.py", "pyproject.toml", ".ruff-lsp.toml", "."),
-}
--- require'lspconfig'.ccls.setup{}
+-- lspconfig.pyright.setup{}
+-- lspconfig.ruff_lsp.setup{
+-- 	cmd = {"ruff-lsp"},
+-- 	root_dir = lspconfig.util.root_pattern(".git", "setup.py", "pyproject.toml", ".ruff-lsp.toml", "."),
+-- }
