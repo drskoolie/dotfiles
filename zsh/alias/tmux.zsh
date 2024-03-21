8b5006f43f165402de74b80a36f02a308ed42596
Al-Baraa El-Hag <a.elhag01@gmail.com>
2024-03-21 09:35:00 -0400

    Add a cool tmux shortcut

--- zsh/alias/tmux.zsh
+++ zsh/alias/tmux.zsh
@@ -4,14 +4,27 @@
 
 function t () 
 {
+	# Get the folder from the second argument
+	local folder_name="$2" 
+
 	case $1 in
 		as) tmux attach-session;;
 		ast) tmux attach-session -t $2;;
 
-		ls) tmux list-sessions;;
+		gt) 
+			cd ~/github/"$folder_name"/
+			source venv_"$folder_name"/bin/activate
+			if tmux has-session -t "$folder_name" 2>/dev/null; then
+				tmux attach-session -t "$folder_name"
+			else
+				tmux new-session -s "$folder_name"
+			fi
+			;;
 
 		ks) tmux kill-session;;
 		kst) tmux kill-session -t $2;;
+
+		ls) tmux list-sessions;;
 
 		ns) tmux new-session;;
 		nss) tmux new-session -s $2;;
@@ -19,5 +32,4 @@ function t ()
 		t) tmux $1;;
 	esac
 }
-
 
