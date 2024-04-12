491d0920ee7e54db18d80ccf8842cd69d17c916b
Al-Baraa El-Hag <a.elhag01@gmail.com>
2024-04-12 15:29:25 -0400

    Remove typst-lsp

--- nvim/lua/lsp/mason/init.lua
+++ nvim/lua/lsp/mason/init.lua
@@ -4,14 +4,8 @@ require("mason-lspconfig").setup {
 	PATH = "prepend",
 	ensure_installed = {
 		"clangd",
-		"cmake",
-		"html",
-		"jsonls",
 		"pyright",
 		"ruff_lsp",
-		"vimls",
-		"hydra_lsp",
-		"typst_lsp",
 	},
 }
 
