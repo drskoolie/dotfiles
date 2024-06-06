aba19cec58d3745412ea4b488b4586dcd03fdd50
Al-Baraa El-Hag <a.elhag01@gmail.com>
2024-06-05 23:04:08 -0400

    Remove python zsh

--- zsh/alias/init.zsh
+++ zsh/alias/init.zsh
@@ -1,7 +1,6 @@
 source ~/dotfiles/zsh/alias/applications.zsh
 source ~/dotfiles/zsh/alias/cd.zsh
 source ~/dotfiles/zsh/alias/general.zsh
-source ~/dotfiles/zsh/alias/python.zsh
 source ~/dotfiles/zsh/alias/nvim.zsh
 source ~/dotfiles/zsh/alias/source.zsh
 source ~/dotfiles/zsh/alias/tmux.zsh
--- zsh/alias/source.zsh
+++ zsh/alias/source.zsh
@@ -2,6 +2,7 @@
 # | source |
 # +--------+
 alias dea='deactivate'
+
 alias sog='source ~/dotfiles/venv/venv_global/bin/activate'
 alias som='source ~/dotfiles/venv/venv_ml/bin/activate'
 alias sop='source venv*/bin/activate'
