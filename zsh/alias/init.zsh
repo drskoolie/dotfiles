0c944b1422bdcd4fb106db2d55793dc9ee86222e
Al-Baraa El-Hag <a.elhag01@gmail.com>
2024-06-05 23:03:36 -0400

    Remove nohup

--- zsh/alias/init.zsh
+++ zsh/alias/init.zsh
@@ -2,7 +2,6 @@ source ~/dotfiles/zsh/alias/applications.zsh
 source ~/dotfiles/zsh/alias/cd.zsh
 source ~/dotfiles/zsh/alias/general.zsh
 source ~/dotfiles/zsh/alias/python.zsh
-source ~/dotfiles/zsh/alias/nohup.zsh
 source ~/dotfiles/zsh/alias/nvim.zsh
 source ~/dotfiles/zsh/alias/source.zsh
 source ~/dotfiles/zsh/alias/tmux.zsh
