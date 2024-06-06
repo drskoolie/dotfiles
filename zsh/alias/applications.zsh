4604e47461de306836592419c1547fb960d7480b
Al-Baraa El-Hag <a.elhag01@gmail.com>
2024-06-05 22:59:14 -0400

    Remove a couple of things

--- zsh/alias/applications.zsh
+++ zsh/alias/applications.zsh
@@ -1,8 +1,6 @@
 # +--------------+
 # | applications |
 # +--------------+
-alias c='sgpt'
-
 alias g='git'
 alias s='scrot --select --line mode=edge ~/media/screenshots/screenshot.png'
 alias S='scrot --select --line mode=edge'
--- zsh/compint.zsh
+++ zsh/compint.zsh
@@ -1,6 +1,2 @@
-# End of lines configured by zsh-newuser-install
-# The following lines were added by compinstall
-zstyle :compinstall filename '/home/pyro/.zshrc'
-
 autoload -Uz compinit
 compinit
