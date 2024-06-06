b9d471f7ce01134c55800c0615bdaeb47feaa787
Al-Baraa El-Hag <a.elhag01@gmail.com>
2024-06-06 18:47:45 -0400

    Delete a lot from sorcery

--- nvim/lua/config/sorcery/init.lua
+++ nvim/lua/config/sorcery/init.lua
@@ -37,24 +37,6 @@ vim.cmd([[set clipboard=unnamedplus]])
 vim.cmd([[com! TmuxPaste lua tmux_paste()]])
 vim.cmd([[com! TmuxSendClear lua tmux_send_clear()]])
 
--- vim.cmd([[com! -nargs=+ TmuxSend lua tmux_send(<q-args>)]])
-
-function tmux_pytest_open()
-	vim.cmd([[silent !tmux new-window -n pytest]])
-end
-
-function tmux_pytest_run()
-	output0 = [[silent !tmux send -t pytest.0 'clear' Enter]]
-	output1 = [[silent !tmux send -t pytest.0 'python3 -m pytest' Enter]]
-	output2 = [[silent !tmux select-window -t pytest]]
-	vim.cmd(output0)
-	vim.cmd(output1)
-	vim.cmd(output2)
-end
-
-vim.cmd([[com! TmuxPytestOpen lua tmux_pytest_open()]])
-vim.cmd([[com! TmuxPytestRun lua tmux_pytest_run()]])
-
 local tmux_kill_pane_last = ':silent !tmux last-pane \\; kill-pane<CR>'
 
 -- block_copy
@@ -102,16 +84,3 @@ function set_trace_up()
 	vim.cmd('normal! iimport ipdb; ipdb.set_trace(context = 10)')
 	vim.cmd('normal! ^')
 end
-
-function tmux_latex_clear()
-	vim.cmd([[silent !tmux sent -t latex.0 'latexmk -c' Enter]])
-end
-
-function tmux_latex_run()
-	vim.cmd([[silent !tmux new-window -n latex]])
-	vim.cmd([[silent !tmux select-window -n -p]])
-	vim.cmd[[silent !tmux send -t latex.0 'latexmk -pvc' Enter]]
-end
-
-vim.cmd([[com! TmuxLatexClear lua tmux_latex_clear()]])
-vim.cmd([[com! TmuxLatexRun lua tmux_latex_run()]])
