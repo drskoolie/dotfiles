dbc15f7e561d092a64d13ab67eb68ebfef75c56a
Al-Baraa El-Hag <a.elhag01@gmail.com>
2024-06-09 10:40:00 -0400

    Fixed neogit fzf-lua dependency

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -167,6 +167,10 @@ require("lazy").setup({
     dependencies = { {'nvim-lua/plenary.nvim'} }
   },
 
+  { 'nvim-telescope/telescope-fzf-native.nvim', 
+     build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' 
+  },
+
   -- +----------+
   -- | whichkey |
   -- +----------+
