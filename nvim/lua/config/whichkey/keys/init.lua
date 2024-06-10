89492b1ae6438d987f449d95be72cc57a72cedfb
Al-Baraa El-Hag <a.elhag01@gmail.com>
2024-06-09 21:09:51 -0400

    Major refactor of which_key

--- nvim/lua/config/whichkey/keys/init.lua
+++ nvim/lua/config/whichkey/keys/init.lua
@@ -1,5 +1,74 @@
 wk_mappings = {
 
+	a = {
+		name = "+apps",
+
+		a = {
+			name = "+accounting",
+
+			b = {':w|!bean-report % balances<CR>', 'balances'},
+			c = {':w|!bean-check %<CR>', 'check'},
+		},
+
+		b = {
+			name = "+boxes",
+
+			b = {'!boxes -a hcvc -d ', 'boxes', mode='v'},
+			c = {'!boxes -a hcvc -d columns<CR>', 'columns', mode='v'},
+			d = {'!boxes -a hcvc -d dog<CR>', 'dog', mode='v'},
+			g = {'!boxes -a hcvc -d girl<CR>', 'girl', mode='v'},
+			i = {'!boxes -a hcvc -d ian jones<CR>', 'ian jones', mode='v'},
+			m = {'!boxes -a hcvc -d mouse<CR>', 'mouse', mode='v'},
+			n = {'!boxes -a hcvc -d spring<CR>', 'spring', mode='v'},
+			p = {'!boxes -a hlvt -d parchment<CR>', 'parchment', mode='v'},
+			s = {'!boxes -a hcvc -d stone<CR>', 'stone', mode='v'},
+			u = {'!boxes -a hcvc -d unicornsay<CR>', 'unicornsay', mode='v'},
+			v = {'!boxes -a hcvc -d unicornthink<CR>', 'unicornthink', mode='v'},
+			y = {'!boxes -a hcvc -d boy<CR>', 'boy', mode='v'},
+		},
+
+		c = {
+			name = "+colorizer",
+
+			a = {':ColorizerAttachToBuffer<CR>',  'attach'},
+			d = {':ColorizerDetachFromBuffer<CR>',  'detach'},
+			p = {'<cmd>PickColor<CR>', 'picker'},
+			r = {':ColorizerReloadAllBuffers<CR>',  'reload'},
+			t = {':ColorizerToggle<CR>',  'toggle'},
+			x = {':XtermColorTable<CR>',  'xterm'},
+		},
+		 
+		g = {
+			name = "+gams",
+
+			m = {':w<CR>:silent !gams %:h/main.gms<CR>', 'main'},
+			M = {':w<CR>:!gams %:h/main.gms<CR>', 'main'},
+			o = {':w<CR>:!gams %:p:t:r.gms > %:p:t:r.out<CR>', 'out'},
+			r = {':w<CR>:!gams %:p:t:r.gms<CR>', 'run'},
+			s = {':w<CR>:silent !gams %:p:t:r.gms > %:p:t:r.out<CR>', 'silent'},
+			w = {':w<CR>:vsp %:p:t:r.lst<CR>G:sp %:p:t:r.out<CR>G', 'window'},
+		},
+
+		l = {':Lazy<CR>', 'lazy'},
+
+		m = {
+			name = "+markdown",
+
+			p = {':MarkdownPreview<CR>', 'preview'},
+			q = {':MarkdownPreviewStop<CR>', 'quit'},
+		},
+
+		t = {':Neotree<CR>', 'neotree'},
+		u = {
+			name = "+undotree",
+
+			f = {':UndotreeFocus<CR>', 'focus'},
+			h = {':UndotreeHide<CR>', 'hide'},
+			s = {':UndotreeShow<CR>', 'show'},
+			t = {':UndotreeToggle<CR>', 'toggle'},
+		},
+	},
+
 	b = {
 		name = "+buffers",
 
@@ -40,47 +109,6 @@ wk_mappings = {
 		h = {':FzfLua helptags<CR>', 'helptags'},
 		j = {':FzfLua jumps<CR>', 'jumps'},
 		k = {':FzfLua keymaps<CR>', 'keymaps'},
-	},
-
-	p = {
-		name = "+dap",
-
-		b = {'<cmd> lua require("dap").toggle_breakpoint()<CR>', 'breakpoint'},
-		B = {'<cmd> lua require("dap").clear_breakpoint()<CR>', 'clear'},
-		c = {'<cmd> lua require("dap").continue()<CR>', 'continue'},
-		g = {'<cmd> lua require("dap").goto_()<CR>', 'goto'},
-		f = {
-			name = "+frame",
-			d = {'<cmd> lua require("dap").down()<CR>', 'down'},
-			f = {'<cmd> lua require("dap").focus_frame()<CR>', 'focus'},
-			r = {'<cmd> lua require("dap").restart_frame()<CR>', 'restart'},
-			u = {'<cmd> lua require("dap").up()<CR>', 'up'},
-		},
-		o = {
-			"+open",
-
-			b = {"<cmd> lua require('lsp/dap/focus_buffer').focus_buffer('DAP Breakpoints')<CR>",'breakpoints'},
-			f = {"<cmd> lua require('lsp/dap/focus_buffer').focus_buffer('DAP Stacks')<CR>",'frame'},
-			h = {"<cmd> lua require('lsp/dap/focus_buffer').focus_buffer('DAP Hover')<CR>",'hover'},
-			r = {"<cmd> lua require('lsp/dap/focus_buffer').focus_buffer('[dap-repl]')<CR>",'repl'},
-			o = {"<cmd> lua require('lsp/dap/focus_buffer').focus_buffer_file()<CR>",'og'},
-			s = {"<cmd> lua require('lsp/dap/focus_buffer').focus_buffer('DAP Scopes')<CR>",'scopes'},
-			t = {"<cmd> lua require('lsp/dap/focus_buffer').focus_buffer('[dap-terminal]')<CR>",'terminal'},
-			w = {"<cmd> lua require('lsp/dap/focus_buffer').focus_buffer('DAP Watches')<CR>",'watches'},
-
-		},
-		r = {'<cmd> lua require("dap").restart()<CR>', 'restart'},
-		s = {
-			name = "+step",
-			b = {'<cmd> lua require("dap").step_back()<CR>', 'back'},
-			c = {'<cmd> lua require("dap").run_to_cursor()<CR>', 'run to cursor'},
-			i = {'<cmd> lua require("dap").step_into()<CR>', 'into'},
-			o = {'<cmd> lua require("dap").step_out()<CR>', 'out'},
-			v = {'<cmd> lua require("dap").step_over()<CR>', 'over'},
-		},
-		t = {'<cmd> lua require("dap").terminate()<CR>', 'terminate'},
-		u = {'<cmd> lua require("dapui").toggle()<CR>', 'ui'},
-		U = {'<cmd> lua require("dapui").open({reset = true})<CR>', 'ui reset'},
 	},
 
 	g = {
@@ -161,29 +189,58 @@ wk_mappings = {
 		a = {':set nrformats+=alpha<CR>', 'alpha+'},
 		A = {':set nrformats-=alpha<CR>', 'alpha-'},
 		e = {':echo expand(\'%:p:h\')<CR>)', 'echo'},
-		l = {':Lazy<CR>', 'lazy'},
 		L = {':w<CR>:luafile %<CR>',  'lua source'},
 		s = {':set spell!<CR>', 'spell toggle'},
-		t = {':Neotree<CR>', 'neotree'},
 		v = {':w<CR>:source %<CR>',  'vim source'},
+	},
+
+	p = {
+		name = "+dap",
+
+		b = {'<cmd> lua require("dap").toggle_breakpoint()<CR>', 'breakpoint'},
+		B = {'<cmd> lua require("dap").clear_breakpoint()<CR>', 'clear'},
+		c = {'<cmd> lua require("dap").continue()<CR>', 'continue'},
+		g = {'<cmd> lua require("dap").goto_()<CR>', 'goto'},
+		f = {
+			name = "+frame",
+			d = {'<cmd> lua require("dap").down()<CR>', 'down'},
+			f = {'<cmd> lua require("dap").focus_frame()<CR>', 'focus'},
+			r = {'<cmd> lua require("dap").restart_frame()<CR>', 'restart'},
+			u = {'<cmd> lua require("dap").up()<CR>', 'up'},
+		},
+		o = {
+			"+open",
+
+			b = {"<cmd> lua require('lsp/dap/focus_buffer').focus_buffer('DAP Breakpoints')<CR>",'breakpoints'},
+			f = {"<cmd> lua require('lsp/dap/focus_buffer').focus_buffer('DAP Stacks')<CR>",'frame'},
+			h = {"<cmd> lua require('lsp/dap/focus_buffer').focus_buffer('DAP Hover')<CR>",'hover'},
+			r = {"<cmd> lua require('lsp/dap/focus_buffer').focus_buffer('[dap-repl]')<CR>",'repl'},
+			o = {"<cmd> lua require('lsp/dap/focus_buffer').focus_buffer_file()<CR>",'og'},
+			s = {"<cmd> lua require('lsp/dap/focus_buffer').focus_buffer('DAP Scopes')<CR>",'scopes'},
+			t = {"<cmd> lua require('lsp/dap/focus_buffer').focus_buffer('[dap-terminal]')<CR>",'terminal'},
+			w = {"<cmd> lua require('lsp/dap/focus_buffer').focus_buffer('DAP Watches')<CR>",'watches'},
+
+		},
+		r = {'<cmd> lua require("dap").restart()<CR>', 'restart'},
+		s = {
+			name = "+step",
+			b = {'<cmd> lua require("dap").step_back()<CR>', 'back'},
+			c = {'<cmd> lua require("dap").run_to_cursor()<CR>', 'run to cursor'},
+			i = {'<cmd> lua require("dap").step_into()<CR>', 'into'},
+			o = {'<cmd> lua require("dap").step_out()<CR>', 'out'},
+			v = {'<cmd> lua require("dap").step_over()<CR>', 'over'},
+		},
+		t = {'<cmd> lua require("dap").terminate()<CR>', 'terminate'},
+		u = {'<cmd> lua require("dapui").toggle()<CR>', 'ui'},
+		U = {'<cmd> lua require("dapui").open({reset = true})<CR>', 'ui reset'},
 	},
 
 	t = {
 		name = "+telescope",
 		d = {":Telescope diagnostics<CR>", 'diagnostics'},
-		i = {":Telescope lsp_implementations<CR>", 'implementations'},
 		s = {":Telescope lsp_document_symbols<CR>", 'document'},
 		w = {":Telescope lsp_workspace_symbols<CR>", 'workspace'},
 		W = {":Telescope lsp_dynamic_workspace_symbols<CR>", 'dynamic'},
-	},
-
-	u = {
-		name = "+undotree",
-
-		f = {':UndotreeFocus<CR>', 'focus'},
-		h = {':UndotreeHide<CR>', 'hide'},
-		s = {':UndotreeShow<CR>', 'show'},
-		t = {':UndotreeToggle<CR>', 'toggle'},
 	},
 
 	z = {
@@ -198,64 +255,6 @@ wk_mappings = {
 		r = {':lua zellij_send_action("zellij action new-pane -d right; ")<CR>', 'right'},
 		y = {":lua zellij_send_ascii(121)<CR>", 'y'},
 	},
-
-	[" "] = {
-		name = "+<SPC>",
-
-		a = {
-			name = "+accounting",
-
-			b = {':w|!bean-report % balances<CR>', 'balances'},
-			c = {':w|!bean-check %<CR>', 'check'},
-		},
-
-		b = {
-			name = "+boxes",
-
-			b = {'!boxes -a hcvc -d ', 'boxes', mode='v'},
-			c = {'!boxes -a hcvc -d columns<CR>', 'columns', mode='v'},
-			d = {'!boxes -a hcvc -d dog<CR>', 'dog', mode='v'},
-			g = {'!boxes -a hcvc -d girl<CR>', 'girl', mode='v'},
-			i = {'!boxes -a hcvc -d ian jones<CR>', 'ian jones', mode='v'},
-			m = {'!boxes -a hcvc -d mouse<CR>', 'mouse', mode='v'},
-			n = {'!boxes -a hcvc -d spring<CR>', 'spring', mode='v'},
-			p = {'!boxes -a hlvt -d parchment<CR>', 'parchment', mode='v'},
-			s = {'!boxes -a hcvc -d stone<CR>', 'stone', mode='v'},
-			u = {'!boxes -a hcvc -d unicornsay<CR>', 'unicornsay', mode='v'},
-			v = {'!boxes -a hcvc -d unicornthink<CR>', 'unicornthink', mode='v'},
-			y = {'!boxes -a hcvc -d boy<CR>', 'boy', mode='v'},
-		},
-
-		c = {
-			name = "+colorizer",
-
-			a = {':ColorizerAttachToBuffer<CR>',  'attach'},
-			d = {':ColorizerDetachFromBuffer<CR>',  'detach'},
-			p = {'<cmd>PickColor<CR>', 'picker'},
-			r = {':ColorizerReloadAllBuffers<CR>',  'reload'},
-			t = {':ColorizerToggle<CR>',  'toggle'},
-			x = {':XtermColorTable<CR>',  'xterm'},
-		},
-		 
-		g = {
-			name = "+gams",
-
-			m = {':w<CR>:silent !gams %:h/main.gms<CR>', 'main'},
-			M = {':w<CR>:!gams %:h/main.gms<CR>', 'main'},
-			o = {':w<CR>:!gams %:p:t:r.gms > %:p:t:r.out<CR>', 'out'},
-			r = {':w<CR>:!gams %:p:t:r.gms<CR>', 'run'},
-			s = {':w<CR>:silent !gams %:p:t:r.gms > %:p:t:r.out<CR>', 'silent'},
-			w = {':w<CR>:vsp %:p:t:r.lst<CR>G:sp %:p:t:r.out<CR>G', 'window'},
-		},
-
-		m = {
-			name = "+markdown",
-
-			p = {':MarkdownPreview<CR>', 'preview'},
-			q = {':MarkdownPreviewStop<CR>', 'quit'},
-		},
-	},	
-
 }
 
 wk_settings.register(wk_mappings, wk_opts)
