c1bc329770390fdf96374081218075ee040e106a
Al-Baraa El-Hag <a.elhag01@gmail.com>
2024-08-28 11:43:44 -0400

    Add Java

--- zsh/applications/init.zsh
+++ zsh/applications/init.zsh
@@ -1,3 +1,4 @@
 source ~/dotfiles/zsh/applications/lf.zsh
+source ~/dotfiles/zsh/applications/java.zsh
 source ~/dotfiles/zsh/applications/yazi.zsh
 source ~/dotfiles/zsh/applications/zoxide.zsh
---
+++ zsh/applications/java.zsh
@@ -0,0 +1,2 @@
+export JAVA_HOME=/usr/local/java_22/
+export PATH=$PATH:$JAVA_HOME/bin
