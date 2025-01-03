a704d811917748c4a6dee9e917b070ca08e61c58
Al-Baraa El-Hag <a.elhag01@gmail.com>
2025-01-03 09:53:55 -0500

    Add a bit more

--- nvim/lua/lsp/mason/init.lua
+++ nvim/lua/lsp/mason/init.lua
@@ -6,11 +6,13 @@ require("mason-lspconfig").setup {
 		"clangd",
 		"pyright",
 		"ruff_lsp",
+		"svelte",
 		-- "rust_analyzer",
 	},
 }
 
-require("lsp/mason/settings/python")
 require("lsp/mason/settings/clangd")
+require("lsp/mason/settings/python")
+require("lsp/mason/settings/svelte")
 -- require("lsp/mason/settings/rust_analyzer")
 -- require("lsp/mason/settings/typst")
