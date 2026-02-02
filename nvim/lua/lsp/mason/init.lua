20b81098f9880e9faceca68bfd0fba8cf0a91c49
Al-Baraa El-Hag <a.elhag01@gmail.com>
2026-02-02 09:06:38 -0500

    Add better python lsp

--- nvim/lua/lsp/mason/init.lua
+++ nvim/lua/lsp/mason/init.lua
@@ -3,13 +3,15 @@ require("mason").setup()
 require("mason-lspconfig").setup {
 	PATH = "prepend",
 	ensure_installed = {
+		"basedpyright",
 		"clangd",
-		"pyright",
 		"ruff",
 		"tinymist",
 	},
 }
 
+require("lsp/mason/settings/basedpyright")
 require("lsp/mason/settings/clangd")
 require("lsp/mason/settings/typst")
-require("lsp/mason/settings/python")
+-- require("lsp/mason/settings/pyright")
+require("lsp/mason/settings/ruff")
